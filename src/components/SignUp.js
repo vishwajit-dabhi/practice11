769f09f3e0f4093bde1162252b9943ef61f1dd88
vishwajit-dabhi <141336080+vishwajit-dabhi@users.noreply.github.com>
2023-08-03 20:23:29 +0530

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,35 +1,58 @@
-import React,{useState} from 'react';
-// import { json } from 'react-router-dom';
-
-const SignUp =()=>{
-    // const [name,setName]=useState("");
-    const [email,setEmail]=useState("");
-    const [mobile,setMobile]=useState("");
-    // const [password,setPassword]=useState("");
-    const collectdata=async()=>{
-        console.warn(email,mobile);
-        let result = await fetch('http://localhost:3000/signUp',{
-            method:'post',
-            body:JSON.stringify({email,mobile}),
-            headers:{
-                'content-type':'application/json'
-            },
-        });
-        result = await result.json()
-        console.warn(result);
-    } 
-
-
-    return(
-        <div className='signup-inputs'>
-            <h1>Register</h1>
-            {/* <input  className="inputBox" type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder='Enter name'/> */}
-            <input className="inputBox" type="text" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='email'/> 
-            <input className="inputBox" type="text" value={mobile} onChange={(e)=>setMobile(e.target.value)} placeholder='Mobile'/> 
-            {/* <input className="inputBox" type = "password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='Enter password'/> */}
-            <button onClick={collectdata} className='button' type='button'>SignUp</button>
-        </div>
-    )
-}
-
+import React, { useState } from "react";
+import axios from 'axios'
+ 
+ 
+const SignUp = () => {
+  const [user, setUser] = useState({
+    email: "",
+    mobile: "",
+  });
+ 
+  const { email, mobile} = user;
+   
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+   
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.post("http://localhost:3000/SignUp",user);
+    alert('Data Inserted');
+    // history.push("/");
+  };
+  return (
+    <div className="container bg-light">
+      <div class="row">  
+       <div className="col-sm-4 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Enter your details</h2>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter email"
+              name="email"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter mobile"
+              name="mobile"
+              value={mobile}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          
+          <button className="btn btn-primary btn-block">submit</button>
+        </form>
+      </div>
+    </div>
+  </div>  
+  );
+};
+ 
 export default SignUp;
